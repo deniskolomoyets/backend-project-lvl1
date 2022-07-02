22709819a0d03ea857ad4428c69fbe6839f05f49
Denis <kolomoetc@gmail.com>
2022-07-02 20:31:49 +0300

    9-step

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import startPrime from '../src/prime.js';
+
+startPrime();
---
+++ src/prime.js
@@ -0,0 +1,18 @@
+import gameEngine from './index.js';
+import getRandomInt from './utils.js';
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return num > 1;
+};
+
+const getGameData = () => {
+  const randomNum = getRandomInt();
+  const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+  return [randomNum, correctAnswer];
+};
+export default () => gameEngine(task, getGameData);
