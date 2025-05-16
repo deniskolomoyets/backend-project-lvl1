cb469e29cd22c7e889794f789756cbe7c65cae13
Denis Kolomoyets <deniskolomoyets@DenisMac.local>
2025-05-16 16:49:27 +0300

    eslint fix-3

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
+import prettier from 'eslint-config-prettier' 
+
 export default [
   // ...
   stylistic.configs.recommended,
@@ -8,10 +14,6 @@ export default [
   {
     languageOptions: {
       globals: globals.node,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
